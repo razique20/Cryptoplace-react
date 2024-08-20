e19e1ef6fb9a1cbc18664c31cf52fbc28e4b274e
Razique Mk <raziquemk2002@gmail.com>
2024-08-20 18:13:34 +0530

    UI part completed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,8 +2,38 @@ import React from 'react'
 import './Navbar.css'
 import logo from "../../assets/logo.png"
 import arrow_icon from "../../assets/arrow_icon.png"
+import { useContext } from 'react'
+
+import { CoinContext } from '../../context/CoinContext'
 
 const Navbar = () => {
+
+    const {setCurrency} = useContext(CoinContext);
+
+
+    const currencyHandler = (event) => {
+        switch (event.target.value){
+            case "usd" : {
+                setCurrency({name:"usd" , symbol:"$"});
+                break;
+            }
+            case "eur" : {
+                setCurrency({name:"eur" , symbol:"€"});
+                break;
+            }
+            case "inr" : {
+                setCurrency({name:"inr" , symbol:"₹"});
+                break;
+            }
+            default : {
+                setCurrency({name:"usd" , symbol:"$"});
+                break;
+            }
+        }
+
+    }
+
+    
   return (
     <div className='navbar'>
         <img className='logo' src={logo} alt="" />
@@ -16,9 +46,9 @@ const Navbar = () => {
         </ul>
 
         <div className='nav-right'>
-            <select>
+            <select onChange={currencyHandler}>
                 <option value="usd">USD</option>
-                <option value="euro">EUR</option>
+                <option value="eur">EUR</option>
                 <option value="inr">INR</option>
 
             </select>
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import CoinContextProvider from "./context/CoinContext.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-    <App />
+      <CoinContextProvider>
+        <App />
+      </CoinContextProvider>
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
