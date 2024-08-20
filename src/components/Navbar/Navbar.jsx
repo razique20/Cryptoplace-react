3f395d25dd789a1028af69556f3f0ea22de12ddd
Razique Mk <raziquemk2002@gmail.com>
2024-08-20 15:04:57 +0530

    intial commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import './Navbar.css'
+import logo from "../../assets/logo.png"
+import arrow_icon from "../../assets/arrow_icon.png"
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <img className='logo' src={logo} alt="" />
+
+        <ul>
+            <li>Home</li>
+            <li>Features</li>
+            <li>Pricing</li>
+            <li>Blog</li>
+        </ul>
+
+        <div className='nav-right'>
+            <select>
+                <option value="usd">USD</option>
+                <option value="euro">EUR</option>
+                <option value="inr">INR</option>
+
+            </select>
+            <button >Sign up  <img src={arrow_icon} alt="" /></button>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Navbar
